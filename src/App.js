4e23cfc00600781eab2553d355e8ba30b9bea2f6
vignesh-s1 <vigneshraj734@gmail.com>
2024-12-02 17:29:49 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import TextField from "@mui/material/TextField";
+import axios from "axios";
 
 function App() {
+  const [foodDetails, setFoodDetails] = useState([]);
+
+  const onSearchFood = (e) => {
+    console.log("value", e?.target?.value);
+    getFoodDetails(e?.target?.value);
+  };
+
+  const getFoodDetails = async (value) => {
+    try {
+      const url = `https://themealdb.com/api/json/v1/1/search.php?s=${value}`;
+      const result = await axios.get(url);
+      setFoodDetails(result?.data?.meals);
+    } catch (error) {
+      console.error("error in getting food details");
+    }
+  };
+
+  const getFoodsList = () =>
+    foodDetails?.map((food) => {
+      
+      return(
+      <div className="food-item">
+        <div className="food-cell">{food?.strMeal}</div>
+        <div className="food-cell">{food?.strInstructions}</div>
+        <img src={food?.strMealThumb} alt={`${food?.strMeal}-image`}/>
+      </div>
+    )});
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Food Details</h1>
+      <TextField label="Search Food" onChange={onSearchFood} />
+      <div className="food-list">{getFoodsList()}</div>
     </div>
   );
 }
